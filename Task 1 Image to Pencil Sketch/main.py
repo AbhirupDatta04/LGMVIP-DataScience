a1708bc5ed2664b1317a21619bf90d2a174cf216
Abhirup Datta <66831444+AbhirupDatta04@users.noreply.github.com>
2021-08-06 21:54:00 +0530

    Initial commit

---
+++ Task 1 Image to Pencil Sketch/main.py
@@ -0,0 +1,26 @@
+import cv2
+
+
+
+
+image = cv2.imread("Lionel-Messi.jpg")
+cv2.imshow("Messi", image)
+cv2.waitKey(0)
+
+gray= cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow("Messi gray",gray)
+cv2.waitKey(0)
+
+inverted= 255 - gray
+cv2.imshow("Inverted", inverted)
+cv2.waitKey(0)
+
+
+blurred = cv2.GaussianBlur(inverted, (21, 21), 0)
+cv2.imshow("Blurred", blurred)
+cv2.waitKey(0)
+
+invertblurred = 255 - blurred
+pencil = cv2.divide(gray, invertblurred, scale=256.0)
+cv2.imshow("Sketch", pencil)
+cv2.waitKey(0)
